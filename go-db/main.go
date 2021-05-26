613d68924c295828c89d2dc7bfb8d6048f0824d0
Yassa Taiseer <yassataiseer@gmail.com>
2021-05-25 21:58:36 -0400

    commit

--- go-db/main.go
+++ go-db/main.go
@@ -1,3 +1,4 @@
+//Here are the tables
 package main
 
 import (
@@ -9,23 +10,23 @@ import (
 
 
 func main(){
-	fmt.Println("Starting db")
-	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1:3306)/golangdb")
+	db,err :=sql.Open("mysql","root:new_password@tcp(127.0.0.1:3306)/golangdb")
 	if err != nil{
 		panic(err)
 	}
 	var query string
-	query = "CREATE TABLE IF NOT EXISTS Users ( Name VARCHAR(500), Password VARCHAR(500))"
+	query = "CREATE TABLE IF NOT EXISTS Users(Name VARCHAR(500),Password VARCHAR(500))"
 	create,err := db.Exec(query)
-	if err != nil {
-        panic(err)
-    }
+	if err != nil{
+		panic(err)
+	}
 	fmt.Println(create)
-	query = "CREATE TABLE IF NOT EXISTS Posts ( Name VARCHAR(500), Post VARCHAR(500))"
+
+	query = "CREATE TABLE IF NOT EXISTS Posts(Name VARCHAR(500),Post VARCHAR(5000))"
 	create,err = db.Exec(query)
-	if err != nil {
-        panic(err)
-    }
+	if err != nil{
+		panic(err)
+	}
 	fmt.Println(create)
-	defer db.Close()
+
 }
---
+++ go-db/part2.go
@@ -0,0 +1,1 @@
+//Here is where we inject data we sql
