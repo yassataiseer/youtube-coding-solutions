6c5b1c25351607c6283856b75627b894a4bfa8ab
Yassa Taiseer <yassataiseer@gmail.com>
2021-05-25 22:21:54 -0400

    commit

--- go-db/part2.go
+++ go-db/part2.go
@@ -1,1 +1,26 @@
 //Here is where we inject data we sql
+//Here are the tables
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func main(){
+	db,err :=sql.Open("mysql","root:new_password@tcp(127.0.0.1:3306)/golangdb")
+	if err != nil{
+		panic(err)
+	}
+
+	exec,err:=  db.Query("INSERT INTO Users (Name,Password) VALUES (?,?)", ("Clark Kent"),("Superman123")) 
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(exec)
+	defer db.Close()
+
+}
