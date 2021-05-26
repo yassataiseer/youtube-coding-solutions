d9415fc1d6d9e20c7e612d0cea8eb3401cfaec29
Yassa Taiseer <yassataiseer@gmail.com>
2021-05-25 21:47:41 -0400

    commit

---
+++ go-db/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func main(){
+	fmt.Println("Starting db")
+	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1:3306)/golangdb")
+	if err != nil{
+		panic(err)
+	}
+	var query string
+	query = "CREATE TABLE IF NOT EXISTS Users ( Name VARCHAR(500), Password VARCHAR(500))"
+	create,err := db.Exec(query)
+	if err != nil {
+        panic(err)
+    }
+	fmt.Println(create)
+	query = "CREATE TABLE IF NOT EXISTS Posts ( Name VARCHAR(500), Post VARCHAR(500))"
+	create,err = db.Exec(query)
+	if err != nil {
+        panic(err)
+    }
+	fmt.Println(create)
+	defer db.Close()
+}
