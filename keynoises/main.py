b58729130e4e1d7403f1f54f1c532e79c8bedc48
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-01 14:32:34 -0400

    made code faster

--- keynoises/main.py
+++ keynoises/main.py
@@ -1,16 +1,18 @@
 from pynput.keyboard import Key,Listener 
 
-from playsound import playsound
-
-
+import time
+from pygame import mixer
 def on_release(key):
     if key==Key.esc:
         return False
 
-
 def on_press(key):
+    seconds = time.time()
     print("{0} pressed".format(key))
-    playsound('audio.mp3')
+    mixer.init()
+    mixer.music.load('audio.mp3')
+    mixer.music.play()
+    print(time.time()-seconds)
     
 
 
