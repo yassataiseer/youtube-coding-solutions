ee65ebfc47040ca296ff7b123dda0d0e4d6a0840
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-02 11:48:05 -0400

    commit

--- keynoises/main.py
+++ keynoises/main.py
@@ -1,20 +1,15 @@
 from pynput.keyboard import Key,Listener 
-
-import time
 from pygame import mixer
 def on_release(key):
     if key==Key.esc:
+    #Check to see if the key is the Escape key
         return False
 
 def on_press(key):
-    seconds = time.time()
-    print("{0} pressed".format(key))
-    mixer.init()
-    mixer.music.load('audio.mp3')
-    mixer.music.play()
-    print(time.time()-seconds)
-    
-
+    print("{0} pressed".format(key)) # Prints of key
+    mixer.init() #initialize mixer
+    mixer.music.load('audio.mp3') #load audio file
+    mixer.music.play() #play the file
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
