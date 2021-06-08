62e018b61da037a8d4a80e5ed872f57e7eb319ad
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-08 15:59:49 -0400

    commit

--- routes/app.py
+++ routes/app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html")####ROUTE 1
 @app.route("/signup")
 def signup():
-    return render_template("signup.html")
+    return render_template("signup.html")###ROUTE 2
 
 app.run( debug=True) 
 
--- routes/tests/test_Users.py
+++ routes/tests/test_Users.py
@@ -1,16 +1,13 @@
-import json
-from flask import Flask, render_template,request,session
 
-from requests.auth import HTTPBasicAuth
+
+
 
 def test_index(app, client):
     del app
-    res = client.get('/')
-    assert res.status_code == 200
+    status = client.get("/")
+    assert status.status_code == 200
 
 def test_signup(app, client):
     del app
-    res = client.get('/signup')
-    assert res.status_code == 200
-
-    
+    status = client.get("/signup")
+    assert status.status_code == 200
