aee88839acb8648d8e0cc9ebe47d741ac776769f
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-12 14:22:19 -0400

    commit

--- go-web-dev/main.go
+++ go-web-dev/main.go
@@ -1,23 +1,25 @@
 package main
-import(
+
+import (
 	"html/template"
 	"net/http"
 )
 
-func login(w http.ResponseWriter, r *http.Request){
-	var tplt = template.Must(template.ParseFiles("templates/login.html"))
-	tplt.Execute(w,nil)	
-}
 
+func login(w http.ResponseWriter, r *http.Request){
+	var tmplt = template.Must(template.ParseFiles("templates/login.html"))
+	tmplt.Execute(w,nil)
+}
 func sign_up(w http.ResponseWriter, r *http.Request){
-	var tplt = template.Must(template.ParseFiles("templates/sign_up.html"))
-	tplt.Execute(w,nil)	
+	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
+	tmplt.Execute(w,nil)
 }
 
 func main(){
-	//Main function simply handles routing
-	http.HandleFunc("/", login) 
-	http.HandleFunc("/sign_up", sign_up)
+
+	http.HandleFunc("/",login)
+	http.HandleFunc("/sign_up",sign_up)
 
 	http.ListenAndServe(":8000",nil)
 }
+
