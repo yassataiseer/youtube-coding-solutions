72deb11ad9d52a4d2bfa4cfdfd7c8e9add5353e3
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-14 11:37:53 -0400

    day3 start

---
+++ go-web-dev/day3/dbbuilder.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main(){
+	db,err:= sql.Open("mysql","root:new_password@tcp(127.0.0.1)/mockweb")
+	if err != nil{
+		panic(err)
+	}
+	var query string
+	query = "CREATE TABLE IF NOT EXISTS Users(Name VARCHAR(500),Password VARCHAR(500))"
+	create,err := db.Exec(query)
+	if err!= nil{
+		panic(err)
+	}
+	fmt.Println(create)
+}
+
+
