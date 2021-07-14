e417cabe119507814574c04f14aecc43c433be26
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-14 13:45:37 -0400

    commit

--- go-web-dev/day3/main.go
+++ go-web-dev/day3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -14,11 +15,28 @@ func sign_up(w http.ResponseWriter, r *http.Request){
 	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
 	tmplt.Execute(w,nil)
 }
-
+func login_user(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()//
+	var Username = r.Form["fname"]
+	var Password = r.Form["Password"]
+	fmt.Println(Username," ",Password)
+	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
+	tmplt.Execute(w,nil)
+}
+func sign_up_user(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()//
+	var Username = r.Form["fname"]
+	var Password = r.Form["Password"]
+	fmt.Println(Username," ",Password)
+	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
+	tmplt.Execute(w,nil)
+}
 func main(){
 
 	http.HandleFunc("/",login)
 	http.HandleFunc("/sign_up",sign_up)
+	http.HandleFunc("/login_user",login_user)
+	http.HandleFunc("/sign_up_user",sign_up_user)
 
 	http.ListenAndServe(":8000",nil)
 }
