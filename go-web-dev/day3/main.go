8676f7b91fc0bca0dca7698c27f7edc8f458192a
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-14 16:07:20 -0400

    commit

--- go-web-dev/day3/main.go
+++ go-web-dev/day3/main.go
@@ -15,29 +15,31 @@ func sign_up(w http.ResponseWriter, r *http.Request){
 	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
 	tmplt.Execute(w,nil)
 }
+func signup_user(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	var Username = r.Form["fname"]
+	var Password = r.Form["Password"]
+	fmt.Println(Username," ",Password)
+
+	var tmplt = template.Must(template.ParseFiles("templates/index.html"))
+	tmplt.Execute(w,nil)
+}
 func login_user(w http.ResponseWriter, r *http.Request){
-	r.ParseForm()//
+	r.ParseForm()
 	var Username = r.Form["fname"]
 	var Password = r.Form["Password"]
 	fmt.Println(Username," ",Password)
-	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
+
+	var tmplt = template.Must(template.ParseFiles("templates/index.html"))
 	tmplt.Execute(w,nil)
 }
-func sign_up_user(w http.ResponseWriter, r *http.Request){
-	r.ParseForm()//
-	var Username = r.Form["fname"]
-	var Password = r.Form["Password"]
-	fmt.Println(Username," ",Password)
-	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
-	tmplt.Execute(w,nil)
-}
+
 func main(){
 
 	http.HandleFunc("/",login)
 	http.HandleFunc("/sign_up",sign_up)
 	http.HandleFunc("/login_user",login_user)
-	http.HandleFunc("/sign_up_user",sign_up_user)
-
+	http.HandleFunc("/signup_user",signup_user)
 	http.ListenAndServe(":8000",nil)
 }
 
