9ddcb8b20c8cc0fa91201caabe6d6a0cc33c7d91
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-16 15:09:01 -0400

    commit

--- go-web-dev/day4/main.go
+++ go-web-dev/day4/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
+	"database/sql"
 	"html/template"
 	"net/http"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 
+func add_user(Username string, password string)bool{
+	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1)/mockweb")
+	if err!= nil{
+		panic(err)
+	}
+	add,err := db.Query("INSERT INTO Users(Name,Password) VALUES (?,?)",(Username),(password))
+	if err!= nil{
+		panic(err)
+	}
+	fmt.Println(add)
+	defer db.Close()
+	return true
+}
+
+func check_user(Username string, password string)bool{
+	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1)/mockweb")
+	if err!= nil{
+		panic(err)
+	}
+	var exists bool
+	var query string
+	query = fmt.Sprintf("SELECT EXISTS(SELECT Name FROM Users WHERE Name='%s' AND Password='%s')",(Username),(password))
+	row := db.QueryRow(query).Scan(&exists)
+	if err!=nil && err!= sql.ErrNoRows{
+		panic(err)
+	}
+	fmt.Println(row)
+	defer db.Close()
+	return exists
+}
 func login(w http.ResponseWriter, r *http.Request){
 	var tmplt = template.Must(template.ParseFiles("templates/login.html"))
 	tmplt.Execute(w,nil)
@@ -20,18 +52,27 @@ func signup_user(w http.ResponseWriter, r *http.Request){
 	var Username = r.Form["fname"]
 	var Password = r.Form["Password"]
 	fmt.Println(Username," ",Password)
+	if(add_user(Username[0],Password[0])){
+		var tmplt = template.Must(template.ParseFiles("templates/index.html"))
+		tmplt.Execute(w,nil)
+	}else{
+		var tmplt = template.Must(template.ParseFiles("templates/error.html"))
+		tmplt.Execute(w,nil)
+	}
 
-	var tmplt = template.Must(template.ParseFiles("templates/index.html"))
-	tmplt.Execute(w,nil)
 }
 func login_user(w http.ResponseWriter, r *http.Request){
 	r.ParseForm()
 	var Username = r.Form["fname"]
 	var Password = r.Form["Password"]
 	fmt.Println(Username," ",Password)
-
-	var tmplt = template.Must(template.ParseFiles("templates/index.html"))
-	tmplt.Execute(w,nil)
+	if(check_user(Username[0],Password[0])){
+		var tmplt = template.Must(template.ParseFiles("templates/index.html"))
+		tmplt.Execute(w,nil)
+	}else{
+		var tmplt = template.Must(template.ParseFiles("templates/error.html"))
+		tmplt.Execute(w,nil)
+	}
 }
 
 func main(){
