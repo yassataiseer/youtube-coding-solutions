17c13cfef3d70c610a8e2b1695c9666a296dbeb9
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-20 11:26:21 -0400

    day stick start

---
+++ go-web-dev/day6/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"database/sql"
+	"html/template"
+	"net/http"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func add_user(Username string, password string)bool{
+	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1)/mockweb")
+	if err!= nil{
+		panic(err)
+	}
+	add,err := db.Query("INSERT INTO Users(Name,Password) VALUES (?,?)",(Username),(password))
+	if err!= nil{
+		panic(err)
+	}
+	fmt.Println(add)
+	defer db.Close()
+	return true
+}
+
+func check_user(Username string, password string)bool{
+	db,err := sql.Open("mysql","root:new_password@tcp(127.0.0.1)/mockweb")
+	if err!= nil{
+		panic(err)
+	}
+	var exists bool
+	var query string
+	query = fmt.Sprintf("SELECT EXISTS(SELECT Name FROM Users WHERE Name='%s' AND Password='%s')",(Username),(password))
+	row := db.QueryRow(query).Scan(&exists)
+	if err!=nil && err!= sql.ErrNoRows{
+		panic(err)
+	}
+	fmt.Println(row)
+	defer db.Close()
+	return exists
+}
+func login(w http.ResponseWriter, r *http.Request){
+	var tmplt = template.Must(template.ParseFiles("templates/login.html"))
+	tmplt.Execute(w,nil)
+}
+func sign_up(w http.ResponseWriter, r *http.Request){
+	var tmplt = template.Must(template.ParseFiles("templates/sign_up.html"))
+	tmplt.Execute(w,nil)
+}
+func signup_user(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	var Username = r.Form["fname"]
+	var Password = r.Form["Password"]
+	fmt.Println(Username," ",Password)
+	if(add_user(Username[0],Password[0])){
+		var tmplt = template.Must(template.ParseFiles("templates/index.html"))
+		user := http.Cookie{
+			Name: "Username", Value: Username[0],
+		}
+		http.SetCookie(w, &user)
+		var cookie = user.Value
+		fmt.Println("COOKIE: "+cookie)
+		tmplt.Execute(w,nil)
+	}else{
+		var tmplt = template.Must(template.ParseFiles("templates/error.html"))
+		tmplt.Execute(w,nil)
+	}
+
+}
+func login_user(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	var Username = r.Form["fname"]
+	var Password = r.Form["Password"]
+	fmt.Println(Username," ",Password)
+	if(check_user(Username[0],Password[0])){
+		var tmplt = template.Must(template.ParseFiles("templates/index.html"))
+		user := http.Cookie{
+			Name: "Username", Value: Username[0],
+		}
+		http.SetCookie(w, &user)
+		var cookie = user.Value
+		fmt.Println("COOKIE: "+cookie)
+		tmplt.Execute(w,nil)
+	}else{
+		var tmplt = template.Must(template.ParseFiles("templates/error.html"))
+		tmplt.Execute(w,nil)
+	}
+}
+
+func main(){
+
+	http.HandleFunc("/",login)
+	http.HandleFunc("/sign_up",sign_up)
+	http.HandleFunc("/login_user",login_user)
+	http.HandleFunc("/signup_user",signup_user)
+	http.ListenAndServe(":8000",nil)
+}
+
