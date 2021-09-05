b1f4af9f20808304249f88f970a0a9592a92feb8
Yassa Taiseer <yassataiseer@gmail.com>
2021-09-05 15:54:27 -0400

    character controller complete

--- gamejam/main.go
+++ gamejam/main.go
@@ -2,7 +2,13 @@ package main
 
 import "github.com/gen2brain/raylib-go/raylib"
 
-
+type Leaf struct {
+	posX int32
+	posY int32
+	width int32
+	height int32
+	Color rl.Color
+  }
 
 
 
@@ -19,21 +25,25 @@ func main() {
 	rl.SetTargetFPS(60)
 	character_img := rl.LoadImage("assets/character.png")
 	character := rl.LoadTextureFromImage(character_img)
+	
+
+
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.DrawTexture(character,x_coords,y_coords,rl.White)
+
 		rl.ClearBackground(rl.RayWhite)
 
-		if rl.IsKeyDown(rl.KeySpace){
+		if rl.IsKeyDown(rl.KeySpace)&&y_coords>0{
 			y_coords-=playerVel
 		}else{
 			y_coords+=5
 		}
-		if rl.IsKeyDown(rl.KeyA){
+		if rl.IsKeyDown(rl.KeyA)&&x_coords>-20{
 			x_coords-=5
 		}
-		if rl.IsKeyDown(rl.KeyD){
+		if rl.IsKeyDown(rl.KeyD)&&x_coords<500{
 			x_coords+=5
 		}
 
