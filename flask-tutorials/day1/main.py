f1a2a28bb427c009f63f2a7827d20430fcf864d8
Yassa Taiseer <yassataiseer@gmail.com>
2021-09-18 10:12:58 -0400

    commit

---
+++ flask-tutorials/day1/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+app.run()
