d05c30df9fef572593ff586582031771b758e66f
Yassa Taiseer <yassataiseer@gmail.com>
2021-09-22 17:32:08 -0400

    COMIT

--- doodlejump/main.go
+++ doodlejump/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import (
+	"github.com/gen2brain/raylib-go/raylib"
+)
 
 
 
@@ -17,14 +19,37 @@ func main() {
 
 	background_img := rl.LoadImage("assets/background.png")
 	rl.ImageResize(background_img,screenWidth,screenHeight)
-	background := rl.LoadTextureFromImage(background_img)
 
+	left_img := rl.LoadImage("assets/playerleft.png")
+	left_char := rl.LoadTextureFromImage(left_img)
+
+	right_img := rl.LoadImage("assets/playerright.png")
+	right_char := rl.LoadTextureFromImage(right_img)
+
+	background := rl.LoadTextureFromImage(background_img)
+	current_player :=  rl.LoadTextureFromImage(right_img)
+	
+	var x_pos int32= 0
+	var y_pos int32 = 0
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
 
 		rl.DrawTexture(background, 0, 0, rl.White)
+		rl.DrawTexture(current_player, x_pos, y_pos, rl.White)
+		if rl.IsKeyDown(rl.KeyA){
+			x_pos-=5
+			current_player = left_char
+		}
+		if rl.IsKeyDown(rl.KeyD){
+			x_pos+=5
+			current_player = right_char
+		}
 
+		
+		
 		rl.EndDrawing()
+		y_pos+=2
 	}
 
 	rl.CloseWindow()
