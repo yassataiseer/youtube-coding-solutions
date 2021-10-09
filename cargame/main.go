43ca57284ce56620a9e55acf2fbc8c867a7e34ce
Yassa Taiseer <yassataiseer@gmail.com>
2021-10-09 14:36:26 -0400

    commit

--- cargame/main.go
+++ cargame/main.go
@@ -1,8 +1,40 @@
 package main
 
 import (
+	"math/rand"
+	"time"
 	"github.com/gen2brain/raylib-go/raylib"
 )
+
+type Cars struct {
+	posX int32
+	posY int32
+	velocity int32
+	color string
+  }
+
+func randomCar() Cars {
+	rand.Seed(time.Now().UnixNano())
+	car_colours:= [3]string{"blue","green","yellow"}
+	car_locations:= [7]int32{1,83,167,250,333,417,501}
+
+	var index int32 = int32(rand.Intn(3))
+	var velocity int32
+	var color string
+	if(car_colours[index]=="green"){
+		color = "green"
+		velocity = 7
+	}else if(car_colours[index]=="yellow"){
+		color = "yellow"
+		velocity = 5
+	}else{
+		color = "blue"
+		velocity = 3
+	}
+	var Xpos int32 = car_locations[int32(rand.Intn(7))]
+	current_car := Cars{Xpos,0,velocity,color}
+	return current_car
+}
 
 func main(){
 	screenWidth := int32(600)
@@ -18,15 +50,54 @@ func main(){
 	rl.ImageResize(redcar_img,int32(82),int32(158))
 	redcar := rl.LoadTextureFromImage(redcar_img)
 
+	bluecar_img := rl.LoadImage("assets/bluecar.png")
+	rl.ImageResize(bluecar_img,int32(82),int32(158))
+	rl.ImageFlipVertical(bluecar_img)
+	bluecar := rl.LoadTextureFromImage(bluecar_img)
+
+	greencar_img := rl.LoadImage("assets/greencar.png")
+	rl.ImageResize(greencar_img,int32(82),int32(158))
+	rl.ImageFlipVertical(greencar_img)
+	greencar := rl.LoadTextureFromImage(greencar_img)
+
+	yellowcar_img := rl.LoadImage("assets/yellowcar.png")
+	rl.ImageResize(yellowcar_img,int32(82),int32(158))
+	rl.ImageFlipVertical(yellowcar_img)
+	yellowcar := rl.LoadTextureFromImage(yellowcar_img)
+
 	var x_coords int32 = 300
 	var y_coords int32 = 450
-
+	var roadY int32 = 0
+	Cars := []Cars{}
+	var times int = 3
+	for times!=0{
+		current_car := randomCar()
+		Cars = append(Cars, current_car)
+		times--
+	}
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.DrawTexture(road,0,0,rl.White)
+		rl.DrawTexture(road,0,roadY,rl.White)
+		roadY++
+		if(roadY==50){roadY=0}
 		rl.DrawTexture(redcar,x_coords,y_coords,rl.White)
 		rl.ClearBackground(rl.RayWhite)
+
+		for index,current_car := range Cars{
+			if(current_car.color=="green"){
+				rl.DrawTexture(greencar, current_car.posX, current_car.posY, rl.White)
+			}else if(current_car.color=="yellow"){
+				rl.DrawTexture(yellowcar, current_car.posX, current_car.posY, rl.White)
+			}else {
+				rl.DrawTexture(bluecar, current_car.posX, current_car.posY, rl.White)
+			}
+			Cars[index].posY += current_car.velocity
+			if (current_car.posY>screenHeight){
+				Cars[index] = randomCar()
+
+			}
+		}
 
 		if rl.IsKeyDown(rl.KeyA)&&x_coords>-40{
 			x_coords-=5
