7201782662ddbaf7f4b91c0cfe70448868db0f92
Yassa Taiseer <yassataiseer@gmail.com>
2021-10-29 10:41:51 -0400

    commit

---
+++ blazortuts/blazortutorials/LoginModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+public class LoginModel{
+    [Required]
+    public string Username{get;set;}
+    [Required]
+    public string Password{get;set;}
+}
+
