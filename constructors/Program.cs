a458410f8b13e517932df14b1727ac76cea1473b
Yassa Taiseer <yassataiseer@gmail.com>
2022-04-23 13:20:28 -0400

    commit

---
+++ constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars NewCar1 = new Cars("Monda Civic",2020,"Black");
+            Console.WriteLine(NewCar1.Name);
+        }
+    }
+    class Cars{
+        public string Name{get;set;}
+        public int Year{get;set;}
+        public string Color{get;set;}
+        public Cars(string CarName,int YearMade,string CarColor){
+            Name = CarName;
+            Year = YearMade;
+            Color = CarColor ;
+        }
+    }
+
+
+}
